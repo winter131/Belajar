f085f31b14c8fce46e7c145c6b170c2a08555406
Jaya Ridho <98460833+winter131@users.noreply.github.com>
2025-04-03 19:39:45 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-document.getElementById("menu-btn").addEventListener("click", function() 
+//code javascript untuk hamburger menu dan banner slider//
+
+document.getElementById("button-menu").addEventListener("click", function()
 {
-    document.getElementById("mobile-menu").classList.toggle("hidden");
+    document.getElementById("mobile-menu").classList.toggle("hidden")
 }
 );
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 3000, // Ganti gambar otomatis setiap 3 detik
+        disableOnInteraction: false,
+    },
+});
